5eac96a4448e0025cc6a4b023a95a6e7276483fe
Viraj Dsouza <virajdanieldsouza@gmail.com>
2023-12-09 14:38:57 +0530

    Code Running after loading payoff function

--- FinanceModel_Scratch.py
+++ FinanceModel_Scratch.py
@@ -10,13 +10,13 @@ import numpy as np
 sp_num_qubits = 3
 
 qmci_library = FunctionLibrary()
-function_generator = FunctionGenerator(function_name="state_loading")
+function_generator =  FunctionGenerator(function_name="state_loading")
 # io = RegisterUserInput(size=3)
 input_dict = function_generator.create_inputs(
     {"io": QUInt[sp_num_qubits], "ind": QUInt[1]}
 )
 
-# print(input_dict)
+print(input_dict)
 
 probabilities = np.linspace(0, 1, 2**sp_num_qubits) / sum(
     np.linspace(0, 1, 2**sp_num_qubits)
@@ -29,23 +29,18 @@ sp_params = StatePreparation(
     probabilities=probabilities, error_metric={"KL": {"upper_bound": 0.00}}
 )
 
-# linear_pauli_rotations_params = LinearPauliRotations(
-#     num_state_qubits=sp_num_qubits, bases="Y", offsets=0.4, slopes=0.5
-# )
-
 sp_output = function_generator.StatePreparation(
-    params=sp_params, strict_zero_ios=False, in_wires={"io": input_dict["io"],"ind": input_dict["ind"]}
-)
+    params=sp_params, strict_zero_ios=False, in_wires={"IN": input_dict["io"]})
 print(sp_output)
-function_generator.set_outputs({"io": sp_output["OUT"]})
+
+function_generator.set_outputs({"io": sp_output["OUT"], "ind": input_dict["ind"]})
 qmci_library.add_function(function_generator.to_function_definition())
 
-# qmci_library = FunctionLibrary()
 function_generator = FunctionGenerator(function_name="amp_load")
 
 io = RegisterUserInput(size=3)
 input_dict = function_generator.create_inputs(
-    {"io": QUInt[sp_num_qubits]}
+    {"io": QUInt[sp_num_qubits], "ind": QUInt[1]}
 )
 amplitude_loading_params = PiecewiseLinearAmplitudeLoading(
     num_qubits=3,
@@ -54,14 +49,14 @@ amplitude_loading_params = PiecewiseLinearAmplitudeLoading(
     rescaling_factor=0.001,
 )
 al_output = function_generator.PiecewiseLinearAmplitudeLoading(
-    params=amplitude_loading_params, strict_zero_ios=False, in_wires=input_dict["io"]
+    params=amplitude_loading_params, strict_zero_ios=False, in_wires={"state":input_dict["io"], "target": input_dict["ind"]}
 )
-# print(al_output)
+print(al_output)
 scaled_expectation_value = 0.7  # Probability of 1 after some execution
 expectation_value = amplitude_loading_params.compute_expectation_value(
     scaled_expectation_value
 )
-function_generator.set_outputs({"io": al_output["state"]})
+function_generator.set_outputs({"io": al_output["state"], "ind": al_output["target"]})
 qmci_library.add_function(function_generator.to_function_definition())
 
 from classiq.builtin_functions import ZGate
@@ -129,11 +124,10 @@ in_wires = function_generator.create_inputs(
 
 oracle_out = function_generator.good_state_oracle(in_wires=in_wires)
 
-# pay-off function loading
-
+pay_off_load_inv= function_generator.amp_load(in_wires={"io": oracle_out["io"], "ind": oracle_out["ind"]})
 
 sps_inverse_out = function_generator.state_loading(
-    in_wires={"io": oracle_out["io"], "ind": oracle_out["ind"]},
+    in_wires={"io": pay_off_load_inv["io"], "ind": pay_off_load_inv["ind"]},
     is_inverse=True,
     should_control=False,
 )
@@ -150,31 +144,67 @@ sps_out = function_generator.state_loading(
     should_control=False,
 )
 
-#pay-off function loading
+pay_off_load= function_generator.amp_load(in_wires={"io": sps_out["io"], "ind": sps_out["ind"]})
 
 global_phase_out = function_generator.UGate(
-    UGate(theta=0, phi=0, lam=0, gam=np.pi), in_wires={"TARGET": sps_out["ind"]}
+    UGate(theta=0, phi=0, lam=0, gam=np.pi), in_wires={"TARGET": pay_off_load["ind"]}
 )
 
-function_generator.set_outputs({"io": sps_out["io"], "ind": global_phase_out["TARGET"]})
+function_generator.set_outputs({"io": pay_off_load["io"], "ind": global_phase_out["TARGET"]})
 
 qmci_library.add_function(function_generator.to_function_definition())
 
-
 #APPLYING AMPLITUDE ESTIMATION (AE) WITH QUANTUM PHASE ESTIMATION (QPE)
+from classiq import Model
+from classiq.builtin_functions import PhaseEstimation
+from classiq.model import Constraints
 
-# from classiq import Model
-# from classiq.builtin_functions import PhaseEstimation
-# from classiq.model import Constraints
+n_qpe = 3
+model = Model(constraints=Constraints(max_width=11))
+model.include_library(qmci_library)
+sp_output = model.state_loading()
+al_output= model.amp_load()
+
+qpe_out = model.PhaseEstimation(
+    params=PhaseEstimation(
+        size=n_qpe, unitary_params=qmci_library.get_function("grover"), unitary="grover"
+    ),
+    in_wires={"io": al_output["io"], "ind": al_output["ind"]},
+)
+
+model.set_outputs({"phase_result": qpe_out["PHASE_ESTIMATION"]})
+qprog = synthesize(model.get_model())
+show(qprog)
+
+
+# import matplotlib.pyplot as plt
 #
-# n_qpe = 3
-# model = Model(constraints=Constraints(max_width=9))
-# model.include_library(qmci_library)
-# sp_output = model.state_loading()
+# from classiq import execute
 #
-# qpe_out = model.PhaseEstimation(
-#     params=PhaseEstimation(
-#         size=n_qpe, unitary_params=qmci_library.get_function("grover"), unitary="grover"
-#     ),
-#     in_wires={"io": sp_output["io"], "ind": sp_output["ind"]},
+# results = execute(qprog).result()
+#
+# from classiq.execution import ExecutionDetails
+#
+# res = results[0].value
+#
+# phases_counts = res.parsed_counts
+#
+# ## mapping between register string to phases
+# phases_counts = dict(
+#     (sampled_state.state["phase_result"] / 2**n_qpe, sampled_state.shots)
+#     for sampled_state in res.parsed_counts
+# )
+#
+# plt.bar(phases_counts.keys(), phases_counts.values(), width=0.1)
+# plt.xticks(rotation=90)
+# plt.show()
+# print("phase with max probability: ", max(phases_counts, key=phases_counts.get))
+#
+# print(
+#     "measured amplitude: ",
+#     np.sin(np.pi * max(phases_counts, key=phases_counts.get)) ** 2,
+# )
+# print(
+#     "exact amplitude: ",
+#     sum(np.sin(0.5 * n / 2 + 0.4 / 2) ** 2 * probabilities[n] for n in range(2**3)),
 # )
